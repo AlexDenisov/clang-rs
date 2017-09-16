e61d89f95d6219bdbb9f414d1edacdb353eccfdb
Kyle Mayes <kyle@mayeses.com>
2017-09-16 13:48:25 -0400

    "Update documentation"

---
+++ 4_0/implementors/std/error/trait.Error.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["clang"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"clang/enum.AlignofError.html\" title=\"enum clang::AlignofError\">AlignofError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"clang/enum.OffsetofError.html\" title=\"enum clang::OffsetofError\">OffsetofError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"clang/enum.SaveError.html\" title=\"enum clang::SaveError\">SaveError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"clang/enum.SizeofError.html\" title=\"enum clang::SizeofError\">SizeofError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"clang/enum.SourceError.html\" title=\"enum clang::SourceError\">SourceError</a>",];
+implementors["glob"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glob/struct.GlobError.html\" title=\"struct glob::GlobError\">GlobError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glob/struct.PatternError.html\" title=\"struct glob::PatternError\">PatternError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
